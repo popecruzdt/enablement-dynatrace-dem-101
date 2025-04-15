b1c10b3de05afa589d5b6989a468d904b044b4f1
popecruzdt <anthony.pope-cruz@dynatrace.com>
2025-04-15 12:36:02 -0500

    mkdocs update

---
+++ docs/6-analyze-logs.md
@@ -0,0 +1,4 @@
+# Analyze Logs
+--8<-- "snippets/send-bizevent/6-analyze-logs.js"
+
+
---
+++ docs/snippets/send-bizevent/6-analyze-logs.js
@@ -0,0 +1,5 @@
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  dynatrace.sendBizEvent('page_load', {"page": "6. Analyze Logs"});
+});
+</script>
