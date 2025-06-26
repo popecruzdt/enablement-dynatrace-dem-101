9b04539b4bd579a43e0aabd42a34f7c47cf2704d
popecruzdt <anthony.pope-cruz@dynatrace.com>
2025-06-26 13:06:30 +0100

    nginx

--- .devcontainer/util/functions.sh
+++ .devcontainer/util/functions.sh
@@ -627,3 +627,18 @@ deployDynatraceOnK8s(){
   # Deploy Dynakube with Cloud Native Full Stack mode
   deployCloudNative
 }
+
+deployAstroshopNginx(){
+  
+  printInfo "Deploying Astroshop Nginx components to 'astroshop' namespace"
+  kubectl apply -f $CODESPACE_VSCODE_FOLDER/.devcontainer/astroshop/astroshop-nginxproxy.yaml -n astroshop
+  
+  printInfo "Exposing Astroshop in your dev.container via NodePort 30200"
+
+  printInfo "Change astroshop-frontendproxy service from LoadBalancer to NodePort"
+  kubectl patch service astroshop-frontendproxy --namespace=astroshop --patch='{"spec": {"type": "NodePort"}}'
+
+  printInfo "Exposing the astroshop-frontendproxy in NodePort 30200"
+  kubectl patch service astroshop-frontendproxy --namespace=astroshop --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30200}]'
+
+}
